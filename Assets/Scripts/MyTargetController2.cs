7ccad0602e57b2d20e403ea2ab2ef281e8c11ef9
pkharat92 <pkharat92@gmail.com>
2019-04-09 10:34:21 -0700

    First commit

---
+++ Assets/Scripts/MyTargetController2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MyTargetController2 : MonoBehaviour {
+
+
+    [SerializeField]
+    private Transform _explosionSpawn;
+    [SerializeField]
+    private GameObject _explosionPrefab;
+    [SerializeField]
+    Text scoreText;
+
+    public int _score;
+
+    private AudioSource MyAudioSrc ;
+
+    private void Start()
+    {
+        _score = 0;
+        scoreText.text = "Score = 0";
+        MyAudioSrc = GetComponent<AudioSource>();
+        MyAudioSrc.playOnAwake = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "CannonBall")
+        {
+
+            var ex = Instantiate(_explosionPrefab, _explosionSpawn.position, Quaternion.identity) as GameObject;
+
+            ex.GetComponent<ParticleSystem>().Play();
+            _score++;
+            scoreText.text = "Score = " + _score;
+            MyAudioSrc.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SleepTimer());
+    }
+
+    IEnumerator SleepTimer()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
